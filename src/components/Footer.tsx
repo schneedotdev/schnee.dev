75d9ec8d044f432a93a27c09f4c91cd719b8cacb
brianschnee <brian.a.schnee@gmail.com>
2023-06-28 16:23:02 -0400

    opaque color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,8 @@ const Footer = () => {
         &copy; Brian Schnee.
       </h5>
       <div className="pointer-events-none fixed bottom-0 right-0 z-10 w-full">
-        <div className="h-10 w-full bg-gradient-to-t from-[#18181aF5]"></div>
-        <div className="pb12 h-10 w-full bg-[#18181aF5] pt-1"></div>
+        <div className="h-10 w-full bg-gradient-to-t from-background-opaque"></div>
+        <div className="pb12 h-10 w-full bg-background-opaque pt-1"></div>
       </div>
     </footer>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
+        "background-opaque": "var(--background-opaque)",
         background: "var(--background)",
         primary: "var(--primary)",
         secondary: "var(--secondary)",
