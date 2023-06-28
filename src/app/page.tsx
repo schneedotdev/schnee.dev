68f82455c4cececd1efda6b5f3f11fcd065ee722
brianschnee <brian.a.schnee@gmail.com>
2023-06-28 00:16:31 -0400

    remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import Experience from "@/components/Experience";
-import Header from "@/components/Header";
 import Main from "@/components/Main";
 import Projects from "@/components/Projects";
 import Summary from "@/components/Summary";
 import Blogs from "@/components/Blogs";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
