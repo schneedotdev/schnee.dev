d7c5f1771fca1be60fd440c1318635aa77ee5d95
brianschnee <brian.a.schnee@gmail.com>
2023-06-28 00:15:54 -0400

    move header/footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,14 +18,14 @@ export default function RootLayout({
       <body className={scope.className}>
         <div className="flex h-full min-h-screen w-full flex-col items-center">
           <div className="container flex h-full w-full max-w-2xl flex-col justify-between px-12 pb-12 text-sm text-secondary-color lg:w-[55%]">
-            <div className="mt-28 w-full lg:mt-36"></div>
             <Header />
+            <div className="mt-28 w-full lg:mt-36"></div>
             {children}
-            <Footer />
             <div className="mb-10 mt-20 h-1 w-full text-xs">
               <span className="border-b-[1px] border-dotted">CC BY-NC 4.0</span>{" "}
               2023 &copy; Brian Schnee.
             </div>
+            <Footer />
           </div>
         </div>
       </body>
