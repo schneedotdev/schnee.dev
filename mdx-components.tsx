53e3e8430fc2295e40451c164bf244ad41995925
brianschnee <brian.a.schnee@gmail.com>
2023-06-30 18:00:18 -0400

    overwrite html components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,21 @@
 import type { MDXComponents } from "mdx/types";
 
+type Children = {
+  children: React.ReactNode;
+};
+
+function h1({ children }: Children) {
+  return <h1 className="text-base text-primary">{children}</h1>;
+}
+
+function h2({ children }: Children) {
+  return <h2 className="text-base text-primary">{children}</h2>;
+}
+
+function p({ children }: Children) {
+  return <p className="mt-1 text-secondary">{children}</p>;
+}
+
 export function useMDXComponents(components: MDXComponents) {
-  return { ...components };
+  return {h1, h2, p, ...components}
 }
