33a3dd0c7c2ea776b6574030acc082ab88a61b17
brianschnee <brian.a.schnee@gmail.com>
2023-06-30 01:11:32 -0400

    projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,15 @@
+import { serif } from "@/lib/fonts";
+
 export default function ProjectPage() {
-  return "projects";
+  return (
+    <>
+      <h1 className={`text-primary ${serif.className} tracking-wide`}>
+        Projects <span className="ml-1 text-lg">🔨</span>
+      </h1>
+      <p className="mt-8 text-secondary">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam pariatur
+        voluptate quae dolor velit, harum quam atque voluptates veniam iusto?
+      </p>
+    </>
+  );
 }
