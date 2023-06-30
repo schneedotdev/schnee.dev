d3278247a0028562cb18933260ea59f7acf35035
brianschnee <brian.a.schnee@gmail.com>
2023-06-30 18:00:47 -0400

    update tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={scope.className}>
         <div className="flex flex-col items-center">
-          <div className="text-secondary container flex max-w-2xl flex-col px-12 pb-12 text-sm">
+          <div className="container flex max-w-2xl flex-col px-12 pb-12 text-sm text-secondary">
             <Header />
             {children}
             <Footer />
