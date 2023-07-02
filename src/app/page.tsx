d3804ac9dc8e235695472d25ceed28850e663672
brianschnee <brian.a.schnee@gmail.com>
2023-07-01 22:47:01 -0400

    wrap main content in main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,16 @@ import Heading from "@/components/Heading";
 export default function Home() {
   return (
     <>
-      <Heading content="Brian Schnee" />
-      <h2 className="mt-4">Full-Stack Software Engineer</h2>
-      <Experience startDate={"2018-01-16"} />
-      <Summary
-        content="Transforming ideas into code, one adventure at a time. Constantly pushing
+      <main>
+        <Heading content="Brian Schnee" />
+        <h2 className="mt-4">Full-Stack Software Engineer</h2>
+        <Experience startDate={"2018-01-16"} />
+        <Summary
+          content="Transforming ideas into code, one adventure at a time. Constantly pushing
       the bounds of my capabilities and finding the best ways to share my
       insights with others."
-      />
+        />
+      </main>
       <Preview />
       <Projects />
     </>
