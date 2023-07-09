8087feff60a3ab413d9f8f20bd3ff08ebd1349fc
brianschnee <brian.a.schnee@gmail.com>
2023-07-09 19:00:32 -0400

    contentlayer

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -18,27 +18,27 @@ const computedFields = {
 
 export const Post = defineDocumentType(() => ({
   name: "Post",
-  filePathPattern: `/**/*.mdx`,
+  filePathPattern: `blogs/**/*.mdx`,
   contentType: "mdx",
   fields: {
     title: {
-      type: string,
+      type: "string",
       required: true,
     },
     date: {
-      type: string,
+      type: "string",
       required: true,
     },
     published: {
-      type: Boolean,
+      type: "boolean",
       default: true,
     },
-    computedFields,
   },
+  computedFields,
 }));
 
 export default makeSource({
-  contentDirPath: "content",
+  contentDirPath: "./content",
   documentTypes: [Post],
   mdx: {
     remarkPlugins: [remarkGfm],
@@ -49,6 +49,8 @@ export default makeSource({
         {
           theme: "github-dark",
           onVisitLine(node) {
+            // Prevent lines from collapsing in `display: grid` mode, and allow empty
+            // lines to be copy/pasted
             if (node.children.length === 0) {
               node.children = [{ type: "text", value: " " }];
             }
