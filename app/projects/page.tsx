669431d59a6efc813155439fc4ba7c8c788ba860
brianschnee <brian.a.schnee@gmail.com>
2023-07-10 18:40:21 -0400

    project images

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Heading from "@/components/ui/Heading";
 import Summary from "@/components/Summary";
 
@@ -8,6 +9,48 @@ export default function ProjectPage() {
       <Summary
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam pariatur
         voluptate quae dolor velit, harum quam atque voluptates veniam iusto?"
+      />
+      <Image
+        className="mt-10 w-full rounded-md"
+        src="/assets/schneedotdev.jpg"
+        width={610}
+        height={610}
+        alt="preview of climbers connect website"
+      />
+      <Image
+        className="mt-10 w-full rounded-md"
+        src="/assets/climbers.jpg"
+        width={610}
+        height={610}
+        alt="preview of climbers connect website"
+      />
+      <Image
+        className="mt-10 w-full rounded-md"
+        src="/assets/portfolio.jpg"
+        width={610}
+        height={610}
+        alt="preview of portfolio website"
+      />
+      <Image
+        className="mt-10 w-full rounded-md"
+        src="/assets/attri.jpg"
+        width={610}
+        height={610}
+        alt="preview of attrienterprises website"
+      />
+      <Image
+        className="mt-10 w-full rounded-md"
+        src="/assets/findadev.jpg"
+        width={610}
+        height={610}
+        alt="preview of findadev website"
+      />
+      <Image
+        className="mt-10 w-full rounded-md"
+        src="/assets/timeato.jpg"
+        width={610}
+        height={610}
+        alt="preview of timeato website"
       />
     </>
   );
