3b65db403550b6a6c8cac422f534a76023d6671c
brianschnee <brian.a.schnee@gmail.com>
2023-07-10 03:13:09 -0400

    make preview dynamic

--- components/Preview.tsx
+++ components/Preview.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import Blog from "./Blog";
+import { allPosts } from "@/.contentlayer/generated";
+import { sortByDate } from "@/lib/utils";
 
-const Preview = () => {
+const Preview = async () => {
+  const blogs = sortByDate(await allPosts.slice(0, 3));
+
   return (
     <div className="mt-20 w-full">
       <h2 className="text-primary">
@@ -9,23 +13,16 @@ const Preview = () => {
       </h2>
 
       <div className="mt-10 grid grid-cols-1 gap-7 sm:grid-cols-3 sm:grid-rows-1">
-        <Blog
-          title="The Types You Need - Learning TypeScript"
-          date="March 13, 2023"
-          slug="the-types-you-need"
-        />
-        <Blog
-          title="How to Learn and Use TypeScript: A Comprehensive Beginner's
-            Guide"
-          date="March 10, 2023"
-          slug="how-to-learn-and-use-typescript-a-comprehensive-beginners-guide"
-        />
-        <Blog
-          title="Rust's most unique memory management features explained -
-            Ownership and Borrowing"
-          date="February 15, 2023"
-          slug="rusts-most-unique-memory-management-features-explained-ownership-and-borrowing"
-        />
+        {blogs.map((blog) => {
+          return (
+            <Blog
+              key={blog.title}
+              title={blog.title}
+              date={blog.date}
+              slug={blog.slug}
+            />
+          );
+        })}
       </div>
     </div>
   );
