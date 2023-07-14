9afb3a39b7bff3a8e47bbb2f8188326deb432d42
brianschnee <brian.a.schnee@gmail.com>
2023-07-14 18:46:15 -0400

    svgr dependency. svg -> component

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,13 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
