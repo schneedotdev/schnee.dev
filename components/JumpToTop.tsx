344eb3b83f0810f30db4b60ca890f498c3cd449a
brianschnee <brian.a.schnee@gmail.com>
2023-07-21 18:08:00 -0400

    jump to top button

--- components/JumpToTop.tsx
+++ components/JumpToTop.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Icon from "./ui/Icon";
 
 const JumpToTop = ({ slug }: { slug: string }) => {
   const [isVisible, setIsVisible] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -21,12 +22,28 @@ const JumpToTop = ({ slug }: { slug: string }) => {
     };
   }, [isVisible]);
 
+  useEffect(() => {
+    let hoverTimer: NodeJS.Timeout;
+
+    if (!isVisible && isHovered) {
+      hoverTimer = setTimeout(() => {
+        setIsHovered(false);
+      }, 3000);
+    }
+
+    return () => {
+      clearTimeout(hoverTimer);
+    };
+  }, [isVisible, isHovered]);
+
   return (
     <a
       href={slug + "#"}
       className={`popup fixed rounded p-4 shadow-md ${
-        isVisible ? "visible" : ""
+        isVisible || isHovered ? "visible" : ""
       }`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
       aria-label="scroll to the top of the page"
     >
       <Icon name="uparrow" />
