0861cab1c3a83ab66e0700847cbb39c0d25d1e57
brianschnee <brian.a.schnee@gmail.com>
2023-07-21 18:16:44 -0400

    update summary

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <Summary
           content="Transforming ideas into code, one adventure at a time. Constantly pushing
       the bounds of my capabilities and finding the best ways to share my
-      insights with others."
+      experience with others."
         />
       </main>
       <Preview />
